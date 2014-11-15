0821f0da84a113db724015a56c2ba3c314956043
FTC Students <test@test.com>
2014-11-15 15:39:39 -0500

    This is stuff.

--- robotDriving.c
+++ robotDriving.c
@@ -1,9 +1,8 @@
 #pragma config(Hubs,  S1, HTMotor,  HTServo,  none,     none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorNXT, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorNXT, openLoop, reversed, encoder)
-#pragma config(Servo,  srvo_S1_C2_1,    servo1,               tServoNone)
-#pragma config(Servo,  srvo_S1_C2_2,    servo2,               tServoNone)
+#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorNXT, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorNXT, openLoop, encoder)
+#pragma config(Servo,  srvo_S1_C2_1,    servo1,               tServoStandard)
+#pragma config(Servo,  srvo_S1_C2_2,    servo2,               tServoStandard)
 #pragma config(Servo,  srvo_S1_C2_3,    servo3,               tServoNone)
 #pragma config(Servo,  srvo_S1_C2_4,    servo4,               tServoNone)
 #pragma config(Servo,  srvo_S1_C2_5,    servo5,               tServoNone)
@@ -12,15 +11,14 @@
 
 #include "JoystickDriver.c" //Joystick is included in this program
 
+
 task main()
 {
-  int threshold = 15; //to avoid unnecessary movement
-  //motor[motorD] = 50;
-
-  while(true) //infinite loop
+ while(true) //infinite loop
   {
     getJoystickSettings(joystick);
 
+    int threshold = 15; //to avoid unnecessary movement
     if(abs(joystick.joy1_y1) > threshold)
     {
       motor[motorD] = joystick.joy1_y1; //y1 controller moves motorD
@@ -38,5 +36,24 @@ task main()
     {
       motor[motorE] = 0;
     }
-	}
+
+    if(joy1Btn(8))                  // If Joy1-Button1 is pressed:
+    {
+    	servo[servo1] = 200;        // changes the position of servo1 to 160
+    }
+    if(joy1Btn(6))
+    {
+      servo[servo1] = 90; //Straigh upwards
+    }
+
+    if(joy1Btn(7))
+    {
+    	servo[servo2] = 25;        //changes the position of servo2 to 160
+    }
+    if(joy1Btn(5))
+    {
+      servo[servo2] = 145; //Straigh upwards
+    }
+
+  }
 }
